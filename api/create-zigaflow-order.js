435248c0f95c8cb61c5905650c232252e6ad967d
Orchard1999 <orchardmelamine@gmail.com>
2026-02-16 15:20:48 +0000

    Update create-zigaflow-order.js

--- api/create-zigaflow-order.js
+++ api/create-zigaflow-order.js
@@ -48,7 +48,7 @@ export default async function handler(req, res) {
       reference: 'ORC-O',
       client_reference: poNumber,
       po_number: poNumber,
-      template_name: 'Job Upload Template',
+      template_name: 'Job API Template',
       estimated_delivery: body.requiredDeliveryDate 
         ? new Date(body.requiredDeliveryDate).toISOString() 
         : null,
